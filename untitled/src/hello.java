e6ce0e48b6c94ec2a17e56be7c9c0ea5a8b58d73
os <liufuling14@163.com>
2019-01-03 15:46:45 +0800

    更新完成20190103

--- untitled/src/hello.java
+++ untitled/src/hello.java
@@ -1,2 +1,33 @@
-public class hello {
+public class hello  implements hello1{
+
+    public void hello1(){
+
+        System.out.println("显示");
+    }
+    public static  void main(String[] args){
+
+        System.out.println("输入输出");
+
+        System.out.println("日期进度");
+        hello m=new hello();
+
+        m.hello1();
+        System.out.println(a);
+        System.out.println(b);
+
+        mammalint1 dd=new mammalint1();
+
+        dd.getgo();
+        dd.setgo();
+
+
+        System.out.println("20190103");
+
+    }
+
+
+
+
+
+
 }
